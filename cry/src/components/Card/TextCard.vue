c77f9654477c2f875ad7c9c1aff6d0ccd79745ac
cry <cryhomewindows@qq.com>
2024-07-22 22:52:23 +0800

    240722B

--- cry/src/components/Card/TextCard.vue
+++ cry/src/components/Card/TextCard.vue
@@ -8,7 +8,7 @@
             <el-divider></el-divider>
             <div class="grid-content">
               <div class="line-brown">
-                {{ item.position }}·{{ item.area }}·{{ item.time }}
+                {{ item.position }} · {{ getAreaName(item.area) }} · {{ item.time }}
               </div>
               <div class="line-black">
               ·{{ item.remark }}
@@ -28,11 +28,11 @@ const prop = defineProps({
   col: Number,
 })
 const totalBlocks = ref([
-  {id:0,title:'1xxxxx奖',time:'20220101',area:1,position:'xxx开发',remark:'aggrfbhgnghfbldfhxdfg'},
-  {id:1,title:'2xxxxx奖',time:'20220101',area:1,position:'xxx开发',remark:'aggrfbhgnghfbldfhxdfg'},
-  {id:2,title:'3xxxxx奖',time:'20220101',area:1,position:'xxx开发',remark:'aggrfbhgnghfbldfhxdfg'},
-  {id:3,title:'4xxxxx奖',time:'20220101',area:1,position:'xxx开发',remark:'aggrfbhgnghfbldfhxdfg'},
-  {id:4,title:'5xxxxx奖',time:'20220101',area:1,position:'xxx开发',remark:'aggrfbhgnghfbldfhxdfg'},
+  {id:0,title:'第十三届蓝桥杯重庆赛区C/C++程序设计大学B组三等奖',time:'2022年',area:1,position:'xxx开发',remark:'aggrfbhgnghfbldfhxdfgaggrfbhgnghfbldfhxdfgaggrfbhgnghfbldfhxdfgaggrfbhgnghfbldfhxdfg'},
+  {id:1,title:'重庆市高校数据库应用程序设计竞赛(重邮赛区)一等奖',time:'2022年',area:1,position:'xxx开发',remark:'aggrfbhgnghfbldfhxdfg'},
+  {id:2,title:'第十二届挑战杯中国大学生创业计划竞赛重庆赛区银奖',time:'2022年',area:1,position:'xxx开发',remark:'aggrfbhgnghfbldfhxdfg'},
+  {id:3,title:'2020年重邮电子商务“创新、创意及创业”挑战赛三等奖',time:'2022年',area:1,position:'xxx开发',remark:'aggrfbhgnghfbldfhxdfg'},
+  {id:4,title:'牛客网-C语言能力认证训练营',time:'2022年',area:1,position:'xxx开发',remark:'aggrfbhgnghfbldfhxdfg'},
 ]); // 定义总块数
 const areaList=ref([
   {label:"院级",value:0},
@@ -41,6 +41,9 @@ const areaList=ref([
   {label:"省级",value:3},
   {label:"国家级",value:4},
 ])
+function getAreaName(value){
+  return areaList.value.find(item=>item.value==value).label
+}
 </script>
 <style lang="scss" scoped>
 .grid-block {
@@ -52,9 +55,20 @@ const areaList=ref([
   box-shadow: var(--primary-text-grey) 0 0 10px;
 }
 .grid-title{
-  
+  text-align: left;
+  color: var(--primary-text-blue);
+  font-weight: bold;
 }
 .grid-content{
-
+  text-align: left;
+  .line-brown{
+    color: var(--primary-text-yellow);
+    width: 100%;
+  }
+  .line-black{
+    color: var(--primary-text-black);
+    word-wrap: break-word; /* 强制换行 */
+    overflow-wrap: break-word; /* 强制换行 */
+  }
 }
 </style>
