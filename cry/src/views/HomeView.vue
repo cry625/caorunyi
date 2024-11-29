13818059748afd22ea59441e7aed8d7e35e49d5c
caory <13350165610@163.com>
2024-11-29 11:28:41 +0800

    241129

--- cry/src/views/HomeView.vue
+++ cry/src/views/HomeView.vue
@@ -1,11 +1,41 @@
 <template>
   <div>
-    <el-button @click="count++">count is: {{ count }}</el-button>
-    <el-button type="primary" @click="count++">count is: {{ count }}</el-button>
-    <el-button type="success" @click="count++">count is: {{ count }}</el-button>
-    <el-button type="warning" @click="count++">count is: {{ count }}</el-button>
-    <el-button type="danger" @click="count++">count is: {{ count }}</el-button>
-    <el-button type="info" @click="count++">count is: {{ count }}</el-button>
+    <div>
+      <el-button @click="count++">count is: {{ count }}</el-button>
+      <el-button type="primary" @click="count++">count is: {{ count }}</el-button>
+      <el-button type="success" @click="count++">count is: {{ count }}</el-button>
+      <el-button type="warning" @click="count++">count is: {{ count }}</el-button>
+      <el-button type="danger" @click="count++">count is: {{ count }}</el-button>
+      <el-button type="info" @click="count++">count is: {{ count }}</el-button>
+    </div>
+    <!-- 输入框 -->
+    <div class="demo-input">
+      <el-input placeholder="Primary Input" v-model="primaryInput" prefix-icon="el-icon-search"
+        class="demo-input-item"></el-input>
+      <el-input placeholder="Success Input" v-model="successInput" prefix-icon="el-icon-success" class="demo-input-item"
+        :class="{ 'el-input--success': true }"></el-input>
+      <!-- 注意：Element Plus 输入框没有内置的warning、danger等类型，这里通过自定义类来模拟 -->
+      <el-input placeholder="Warning Input" v-model="warningInput" prefix-icon="el-icon-warning-outline"
+        class="demo-input-item warning-input"></el-input>
+      <el-input placeholder="Danger Input" v-model="dangerInput" prefix-icon="el-icon-error"
+        class="demo-input-item danger-input"></el-input>
+      <el-input placeholder="Info Input" v-model="infoInput" prefix-icon="el-icon-info"
+        class="demo-input-item"></el-input>
+      <el-input placeholder="Default Input" v-model="defaultInput" class="demo-input-item"></el-input>
+    </div>
+
+    <!-- 选择器 -->
+    <div class="demo-select">
+      <el-select v-model="primarySelect" placeholder="Select Primary">
+        <el-option label="Option1" value="1"></el-option>
+        <el-option label="Option2" value="2"></el-option>
+      </el-select>
+      <el-select v-model="successSelect" placeholder="Select Success" class="success-select">
+        <el-option label="OptionA" value="a"></el-option>
+        <el-option label="OptionB" value="b"></el-option>
+      </el-select>
+      <!-- 其余选择器类似，但Element Plus选择器没有内置的warning、danger等类型，可以通过自定义样式实现 -->
+    </div>
     <h1>这是一个基于vue+vite+elementPlus的个人网站</h1>
     <ul>以下介绍本网站中使用的技术统计
       <li>简单描述网站内容</li>
@@ -22,7 +52,7 @@
 <script setup>
 import EchartsItem from '@/components/EchartsItem.vue';
 import { ref } from 'vue';
-const count=ref(0)
+const count = ref(0)
 // 数据
 const pieData = ref([
   { value: 335, name: '直接访问' },
@@ -56,6 +86,16 @@ const lineData = ref([
   { value: 34, name: 'kind5' },
   { value: 88, name: 'kind6' },
 ]);
+
+const primaryInput = ref('');
+const successInput = ref('');
+const warningInput = ref('');
+const dangerInput = ref('');
+const infoInput = ref('');
+const defaultInput = ref('');
+ 
+const primarySelect = ref('');
+const successSelect = ref('');
 </script>
 
 <style scoped lang="css"></style>
