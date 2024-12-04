66271caea915004c0ffc75392646a1c55d045557
cry <cryhomewindows@qq.com>
2024-12-04 22:24:06 +0800

    update elementplus

--- cry/src/views/WorkExp.vue
+++ cry/src/views/WorkExp.vue
@@ -1,7 +1,31 @@
 <template>
-  <div class="container">workexp</div>
+  <el-button type="primary" @click="isShow=true">count is: {{ count }}</el-button>
+  <el-dialog v-model="isShow">
+    <div>hhh</div>
+    <template #footer>
+      <div class="dialog-footer">
+        <el-button @click="isShow = false">Cancel</el-button>
+        <el-button type="primary" @click="func()">
+          Confirm
+        </el-button>
+      </div>
+    </template>
+  </el-dialog>
   </template>
   <script setup>
+ import { ElMessageBox } from 'element-plus';
+import { ref } from 'vue';
+  const isShow=ref(false)
+  function func(){
+    ElMessageBox.confirm(
+      '是否删除','警告',
+      {
+        confirmButtonText:'确定',
+        cancelButtonText:'取消',
+        type:'warning'
+      }
+    ).then(()=>{console.log("hhhh")}).catch(()=>{console.log("wwww")})
+  }
   </script>
   <style scoped lang="css">
   </style>
